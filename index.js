74bd83b52f858f4b351231d596e0999251ace9ad
Ola Christian Gundelsby <lug00ber@gmail.com>
2022-03-21 01:35:38 +0100

    fix: async initalizing of services

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import { calcRealBpm } from './lib/helpers/tempoCalculations.js';
-import { networkServices, registry } from './lib/index.js';
+import initialize from './lib/index.js';
 import { getLogger } from './lib/util/logger.js';
 
 const log = getLogger('main');
 
-const beatTracker = networkServices.beatTrackingService;
+initialize().then(({ NetworkServices, registry }) => {
+	const beatTracker = NetworkServices.beatTrackingService;
 
-beatTracker.on('beat', ({ packet }) => {
-	log('Beat:', packet);
-	log('Real BPM', { bpm: calcRealBpm(packet.bpm, packet.pitch) });
+	beatTracker.on('beat', ({ packet }) => {
+		log('Beat:', packet);
+		log('Real BPM', { bpm: calcRealBpm(packet.bpm, packet.pitch) });
+	});
+
+	registry.on('connected', ({ device }) => {
+		log('Device connected', { ...device });
+	});
+
+	registry.on('disconnected', ({ device }) => {
+		log('Device disconnected', {
+			name: device.name,
+			address: device.macAddress
+		});
+	});
+
+	log('Registered listeners on registry', registry);
 });
-
-registry.on('connected', ({ device }) => {
-	log('Device connected', { ...device });
-});
-
-registry.on('disconnected', ({ device }) => {
-	log('Device disconnected', { name: device.name, address: device.macAddress });
-});
-
-log('Registered listeners on registry', registry);
